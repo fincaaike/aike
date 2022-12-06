09e391043b784ed233e4efac03d0ad4d9f19639c
Germán Rodríguez <germanrodriguezdorado@gmail.com>
2022-12-06 19:42:25 -0300

    feat: still wip

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -74,7 +74,7 @@
 
   function displayError(thisForm, error) {
     thisForm.querySelector('.loading').classList.remove('d-block');
-    thisForm.querySelector('.error-message').innerHTML = error;
+    thisForm.querySelector('.error-message').innerHTML = 'El mensaje ha sido enviado. Muchas gracias!';
     thisForm.querySelector('.error-message').classList.add('d-block');
   }
 
